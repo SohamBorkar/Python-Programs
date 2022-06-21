0d1d8f5f719fecf80a3e1e9ffb9f62009c6fea88
Soham <sohamborkar14@gmail.com>
2022-06-21 23:46:06 +0530

    changes

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,12 @@
+# print(j[1])
+# print(j[2])
 
-    # print(j[1])
-    # print(j[2])
+l=list()
+#s=set()
+print("Enter number of element you want to add: ")
+list=int(input())
+
+for i in range(0,list):
+    print("Enter the element: ")
+    l.append (input())
+print(l)
